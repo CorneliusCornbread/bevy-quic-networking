8e1de9537dcda20462e773fcbca90cc1c9ded220
CorneliusCorn <jrosenbeckerdev@gmail.com>
2025-01-07 04:53:32 -0600

    Gonna abstract most of the rest of this into ECS systems

--- src/client.rs
+++ src/client.rs
@@ -1,22 +1,26 @@
 use std::{error::Error, net::SocketAddr, sync::Arc};
 
-use bevy::prelude::{Resource, World};
+use bevy::{
+    log::error,
+    prelude::{Resource, World},
+};
 use s2n_quic::{client::Connect, Client};
-use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio::sync::mpsc::{self, error::TryRecvError, Receiver, Sender};
 
 use crate::{common::ConnectionState, TokioRuntime};
+
+const NEW_CONN_BATCH_SIZE: usize = 5;
 
 #[derive(Resource)]
 pub struct QuicClient {
     runtime: tokio::runtime::Handle,
-    connections: Vec<SocketAddr>,
     client: Arc<Client>,
-    connection_rx: Receiver<ConnectionState>,
-    connection_tx: Sender<ConnectionState>,
+    conn_receiver: Receiver<ConnectionState>,
+    conn_sender: Arc<Sender<ConnectionState>>,
 }
 
 impl QuicClient {
-    fn init_client(world: &mut World) -> Result<(), Box<dyn Error>> {
+    pub fn init_client(world: &mut World) -> Result<(), Box<dyn Error>> {
         // TODO: TLS certificate support
         let client = Client::builder().with_io("0.0.0.0:0")?.start()?;
         let (tx, rx) = mpsc::channel(1);
@@ -26,23 +30,52 @@ impl QuicClient {
                 .get_resource_or_init::<TokioRuntime>()
                 .handle()
                 .clone(),
-            connections: Vec::new(),
             client: Arc::new(client),
-            connection_rx: rx,
-            connection_tx: tx,
+            conn_receiver: rx,
+            conn_sender: Arc::new(tx),
         };
 
         world.insert_resource(quic_client);
         Ok(())
     }
 
-    fn try_connect(&mut self, addr: SocketAddr) {
+    pub fn try_connect(&mut self, addr: SocketAddr) {
         let connect = Connect::new(addr);
         let client = self.client.clone();
+        let sender = self.conn_sender.clone();
         let runtime = self.runtime.clone();
 
         runtime.spawn(async move {
-            let connection = client.connect(connect).await;
+            let connection: ConnectionState = client.connect(connect).await.into();
+            let _ = sender.send(connection).await;
         });
+    }
+
+    pub fn handle_connections(&mut self) -> [Option<ConnectionState>; NEW_CONN_BATCH_SIZE] {
+        let rec = &mut self.conn_receiver;
+        let mut flag = false;
+        let data: [Option<ConnectionState>; NEW_CONN_BATCH_SIZE] = std::array::from_fn(|_| {
+            // Avoids fragmentation of the returned data.
+            // First none we encounter means we initialize rest of the data with None.
+            if flag {
+                return None;
+            }
+
+            let opt = rec.try_recv();
+
+            match opt {
+                Ok(state) => Some(state),
+                Err(e) => {
+                    match e {
+                        TryRecvError::Empty => (), // Do nothing we don't care,
+                        TryRecvError::Disconnected => error!("Connection sender pipe was broken. Was the sender or receiver dropped/closed before the client structure was dropped?"),
+                    }
+                    flag = true;
+                    None
+                }
+            }
+        });
+
+        data
     }
 }
--- src/common.rs
+++ src/common.rs
@@ -1,10 +1,19 @@
-use std::{error::Error, sync::Arc};
-
 use s2n_quic::Connection;
+use std::error::Error;
 
 #[derive(Debug)]
 pub enum ConnectionState {
     Connected(Connection),
     Failed(Box<dyn Error + Send>),
     InProgress,
+}
+
+impl From<Result<Connection, s2n_quic::connection::Error>> for ConnectionState {
+    fn from(value: Result<Connection, s2n_quic::connection::Error>) -> Self {
+        if let Ok(conn) = value {
+            return Self::Connected(conn);
+        }
+
+        Self::Failed(Box::new(value.unwrap_err()))
+    }
 }
