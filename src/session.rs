a983b45fefff17daf012b5648a451b749f6da9b6
CorneliusCorn <jrosenbeckerdev@gmail.com>
2025-05-29 23:13:05 -0500

    Receive task mostly done

--- src/session.rs
+++ src/session.rs
@@ -1,29 +1,35 @@
 use std::error::Error;
 
-use aeronet::io::bytes::Bytes;
+use aeronet::io::{bytes::Bytes, packet::RecvPacket};
 use bevy::{
     ecs::component::Component,
     log::{error, info, warn},
 };
-use s2n_quic::stream::{BidirectionalStream, ReceiveStream, SendStream, SplittableStream};
+use s2n_quic::{
+    application::Error as ErrorCode,
+    stream::{BidirectionalStream, ReceiveStream, SendStream},
+};
 use tokio::{
     runtime::Handle,
     sync::mpsc::{self, error::TrySendError, Receiver, Sender},
     task::JoinHandle,
-    time::Instant,
+    time::Instant as TokioInstant,
 };
 
 use crate::common::StreamId;
 
 const CHANNEL_BUFF_SIZE: usize = 256;
+const CONNECTION_CLOSED_ERR_CODE: u64 = 200;
 
 pub type QuicSession = QuicSessionInternal;
 
-struct Packet(Bytes, Instant);
-
-enum ControlMessage {
+enum SendControlMessage {
     CloseAndQuit,
     Flush,
+}
+
+enum RecControlMessage {
+    StopSend(ErrorCode),
 }
 
 #[derive(Component)]
@@ -31,10 +37,10 @@ pub(crate) struct QuicSessionInternal {
     runtime: Handle,
     send_task: JoinHandle<()>,
     rec_task: JoinHandle<()>,
-    outbound: Sender<Packet>,
-    inbound: Receiver<Packet>,
-    outbound_control: Sender<ControlMessage>,
-    inbound_control: Sender<ControlMessage>,
+    outbound: Sender<Bytes>,
+    inbound: Receiver<RecvPacket>,
+    outbound_control: Sender<SendControlMessage>,
+    inbound_control: Sender<RecControlMessage>,
     send_errors: Receiver<Box<dyn Error + Send + Sync>>,
     receive_errors: Receiver<Box<dyn Error + Send + Sync>>,
     pub(crate) id: StreamId,
@@ -83,8 +89,8 @@ impl QuicSessionInternal {
 
 async fn outbound_send_task(
     mut send: SendStream,
-    mut control: Receiver<ControlMessage>,
-    mut outbound_receiver: Receiver<Packet>,
+    mut control: Receiver<SendControlMessage>,
+    mut outbound_receiver: Receiver<Bytes>,
     send_errors: Sender<Box<dyn Error + Send + Sync>>,
 ) {
     let mut command_buf = Vec::new();
@@ -103,7 +109,7 @@ async fn outbound_send_task(
             }
 
             match command_opt.unwrap() {
-                ControlMessage::CloseAndQuit => {
+                SendControlMessage::CloseAndQuit => {
                     match send.close().await {
                         Ok(_) => info!("Send stream closed, exiting send stream task..."),
                         Err(e) => {
@@ -112,7 +118,7 @@ async fn outbound_send_task(
                     }
                     break_flag = true;
                 }
-                ControlMessage::Flush => {
+                SendControlMessage::Flush => {
                     let res = send.flush().await;
                     if let Err(e) = res {
                         send_errors.try_send(Box::new(e)).handle_err();
@@ -138,11 +144,11 @@ async fn outbound_send_task(
 
 async fn send_data(
     send: &mut SendStream,
-    outbound_receiver: &mut Receiver<Packet>,
+    outbound_receiver: &mut Receiver<Bytes>,
     send_errors: &Sender<Box<dyn Error + Send + Sync + 'static>>,
 ) {
     if let Some(data) = outbound_receiver.recv().await {
-        let err_opt = send.send(data.0).await.err();
+        let err_opt = send.send(data).await.err();
 
         if let Some(err) = err_opt {
             send_errors.try_send(Box::new(err)).handle_err();
@@ -151,11 +157,65 @@ async fn send_data(
 }
 
 async fn rec_task(
-    rec: ReceiveStream,
-    control: Receiver<ControlMessage>,
-    inbound_sender: Sender<Packet>,
-    mut receive_errors: Sender<Box<dyn Error + Send + Sync>>,
+    mut rec: ReceiveStream,
+    mut control: Receiver<RecControlMessage>,
+    inbound_sender: Sender<RecvPacket>,
+    receive_errors: Sender<Box<dyn Error + Send + Sync>>,
 ) {
+    let mut command_buf = Vec::new();
+
+    const BUFF_SIZE: usize = 100;
+    let mut read_buf: [Bytes; BUFF_SIZE] = std::array::from_fn(|_| Bytes::new());
+
+    'running: loop {
+        let mut break_flag = false;
+
+        let command_count = control.recv_many(&mut command_buf, 100).await;
+
+        for command in command_buf[..command_count].iter() {
+            match command {
+                RecControlMessage::StopSend(code) => {
+                    match rec.stop_sending(*code) {
+                        Ok(_) => info!("Send stream closed, exiting send stream task..."),
+                        Err(e) => {
+                            warn!("Send stream errored when closing: {e}\n Exiting send stream task...")
+                        }
+                    }
+                    break_flag = true;
+                }
+            }
+        }
+
+        let data_res = rec.receive_vectored(&mut read_buf).await;
+
+        match data_res {
+            Ok((size, is_open)) => {
+                let instant = TokioInstant::now();
+                for data in read_buf[0..size].iter() {
+                    let packet = RecvPacket {
+                        recv_at: instant.into_std(),
+                        payload: data.clone(),
+                    };
+
+                    inbound_sender.try_send(packet).handle_err();
+                }
+
+                if !is_open {
+                    let id = rec.id();
+
+                    info!("Receive stream id {id} was closed, quitting receive task...");
+                    break_flag = true;
+                }
+            }
+            Err(rec_err) => {
+                receive_errors.try_send(Box::new(rec_err)).handle_err();
+            }
+        }
+
+        if break_flag {
+            break 'running;
+        }
+    }
 }
 
 trait HandleChannelError {
@@ -165,7 +225,7 @@ trait HandleChannelError {
 impl<T> HandleChannelError for Result<(), TrySendError<T>> {
     fn handle_err(&self) {
         if let Err(send_err) = self {
-            error!("Error buffer for send task is full, the following error will be dropped: {send_err}");
+            error!("Error buffer for async task is full, the following error will be dropped: {send_err}");
         }
     }
 }
